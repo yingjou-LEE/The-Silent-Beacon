9b5db549851f5bcfaf0367eb8e6c6e102999e7d2
King Zhou <zhouyimingking@outlook.com>
2024-10-13 12:14:57 -0700

    basic boat movement

---
+++ Assets/Scripts/BoatController.cs
@@ -0,0 +1,39 @@
+using System;
+using Cinemachine;
+using UnityEngine;
+
+public class BoatController : MonoBehaviour
+{
+    [SerializeField] private float forwardAcceleration = 1;
+    [SerializeField] private float rotationAcceleration = 12;
+    [SerializeField] private CinemachineVirtualCamera boatmanCam;
+    [SerializeField] private Vector2 cameraFovRange = new Vector2(60, 90);
+    [SerializeField] private float maxFovSpeed = 3;
+
+    private Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        InputManager.Instance.OnArrowKeys += Move;
+    }
+
+    private void Update()
+    {
+        boatmanCam.m_Lens.FieldOfView =
+            Mathf.Lerp(cameraFovRange.x, cameraFovRange.y, _rb.velocity.magnitude / maxFovSpeed);
+    }
+
+    private void Move(Vector2 input)
+    {
+        var forwardSpeed = input.y * forwardAcceleration * Time.deltaTime;
+        var rotationSpeed = input.x * rotationAcceleration * Time.deltaTime;
+        
+        _rb.AddForce(transform.forward * forwardSpeed, ForceMode.Force);
+        _rb.AddTorque(Vector3.up * rotationSpeed, ForceMode.Force);
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance { get; private set; }
+
+    public event Action<Vector2> OnWASD;
+    public event Action<Vector2> OnArrowKeys;
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        HandleWASDInput();
+        HandleArrowKeysInput();
+    }
+
+    private void HandleWASDInput()
+    {
+        float moveX = Input.GetAxis("HorizontalWASD");
+        float moveY = Input.GetAxis("VerticalWASD");
+        
+        var input = new Vector2(moveX, moveY);
+        
+        OnWASD?.Invoke(input);
+    }
+    
+    private void HandleArrowKeysInput()
+    {
+        float moveX = Input.GetAxis("HorizontalArrow");
+        float moveY = Input.GetAxis("VerticalArrow");
+        
+        var input = new Vector2(moveX, moveY);
+        
+        OnArrowKeys?.Invoke(input);
+    }
+}
